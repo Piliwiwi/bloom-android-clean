b9b161da73825534c55a00f74589354bcf5d1d86
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-04-02 17:06:30 -0300

    implement crashlytics

--- app/src/main/java/cl/minkai/bloom/login/data/LoginDataRepository.kt
+++ app/src/main/java/cl/minkai/bloom/login/data/LoginDataRepository.kt
@@ -3,6 +3,7 @@ package cl.minkai.bloom.login.data
 import cl.minkai.bloom.login.data.remote.model.RemoteUserCredentialsParams
 import cl.minkai.bloom.login.data.source.LoginCache
 import cl.minkai.bloom.login.data.source.LoginRemote
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
@@ -13,6 +14,7 @@ class LoginDataRepository @Inject constructor(
     fun login(credentials: RemoteUserCredentialsParams) = flow {
         val auth = remote.login(credentials)
         cache.storeToken(auth.token)
+        FirebaseCrashlytics.getInstance().setUserId(credentials.email)
         emit(auth)
     }
 }
