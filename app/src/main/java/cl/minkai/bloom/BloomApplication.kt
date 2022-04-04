d40ffd1600470b92d58f4fe1aaa205532c79ec3c
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-04-03 22:20:24 -0400

    feature flags added

--- app/src/main/java/cl/minkai/bloom/BloomApplication.kt
+++ app/src/main/java/cl/minkai/bloom/BloomApplication.kt
@@ -3,6 +3,8 @@ package cl.minkai.bloom
 import android.app.Application
 import cl.minkai.bloom.common.di.ApplicationComponent
 import cl.minkai.bloom.common.di.DaggerApplicationComponent
+import cl.minkai.bloom.common.featureflags.FeatureFlags
+import cl.minkai.bloom.common.featureflags.FirebaseRemoteConfig
 
 class BloomApplication : Application() {
     val appComponent: ApplicationComponent by lazy {
@@ -15,4 +17,12 @@ class BloomApplication : Application() {
             application = this,
             context = applicationContext
         )
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseRemoteConfig.initRemoteConfig(
+            FeatureFlags.getDefaults(),
+            BuildConfig.DEBUG
+        )
+    }
 }
---
+++ app/src/main/java/cl/minkai/bloom/common/featureflags/FirebaseRemoteConfig.kt
@@ -0,0 +1,25 @@
+package cl.minkai.bloom.common.featureflags
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+
+object FirebaseRemoteConfig {
+    private const val PROD_INTERVAL = 1200L
+
+    fun initRemoteConfig(
+        defaults: Map<String, Any>,
+        isDebug: Boolean = true,
+        fetchInterval: Long = 60L
+    ) {
+        Firebase.remoteConfig.setDefaultsAsync(defaults)
+        fetchConfig(isDebug, fetchInterval)
+    }
+
+    private fun fetchConfig(isDebug: Boolean, fetchInterval: Long) {
+        val settings = FirebaseRemoteConfigSettings.Builder()
+        settings.minimumFetchIntervalInSeconds = if (isDebug) fetchInterval else PROD_INTERVAL
+        Firebase.remoteConfig.setConfigSettingsAsync(settings.build())
+        Firebase.remoteConfig.fetchAndActivate()
+    }
+}
